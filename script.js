7a9b0621fc4ded73cd5711add842c31be262641a
Xavier <xavieralves162@gmail.com>
2025-11-21 12:27:22 +0000

    Added gameboard

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const Gameboard = (function () {
+    const board = ["", "", "", "", "", "", "", "", ""];
+
+    const getBoard = () => board;
+
+    const setCell = (index, marker) => {
+        if (board[index] === "") {
+            board[index] = marker;
+            return true;
+        }
+        return false;
+    };
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++) board[i] = "";
+    };
+
+    return { getBoard, setCell, reset };
+})();
 
